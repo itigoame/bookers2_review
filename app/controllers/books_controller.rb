a370901c7c621036c2aea8550574bea1b0d4ca51
itigoame <rikuan1128@gmail.com>
2022-09-04 08:01:12 +0000

    [add]index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,41 @@
 class BooksController < ApplicationController
-  def new
+
+  def create
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    @books = Book.all
+    @user = current_user
+    if @book_new.save
+      redirect_to book_path(@book_new.id)
+    else
+      render :index
+    end
   end
 
   def show
+    @book=Book.find(params[:id])
+    @user = @book.user.id
+    @book_new = Book.new
+  end
+
+  def index
+    @books = Book.all
   end
 
   def edit
+    @book_new = Book.new
+  end
+
+  def update
+
   end
 
   def destroy
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,35 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
+
+    @book_new = Book.new
   end
 
   def index
     @users = User.all
+
     @user = current_user
+    @book_new = Book.new
   end
 
   def edit
     @user=User.find(params[:id])
     unless @user == current_user
-      redirect_to users_path
+      redirect_to user_path(current_user.id)
     end
   end
 
   def update
     @user = User.find(params[:id])
     if @user != current_user
-      redirect_to users_path
+      redirect_to users_path(@user.id)
     else
-      @user.update(user_params)
-      redirect_to edit_user_path(@user.id)
+      if @user.update(user_params)
+        redirect_to user_path(@user.id)
+      else
+        render :edit
+      end
     end
   end
 
