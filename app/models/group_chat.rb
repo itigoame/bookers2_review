5c9903d440077027d81820dd81fc982ee939b866
itigoame <rikuan1128@gmail.com>
2022-09-30 05:20:41 +0000

    chat

--- app/models/group_chat.rb
+++ app/models/group_chat.rb
@@ -2,5 +2,5 @@ class GroupChat < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-   validates :comment_text, presence: true,length: {maximum:140}
+   validates :body, presence: true,length: {maximum:140}
 end
